55f4b5c6b71200bf21af2fef13552533b740eb78
EdenEden2001 <144505410+EdenEden2001@users.noreply.github.com>
2023-09-09 10:54:10 +0800

    Update Config.js

--- config/Config.js
+++ config/Config.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
     BASE_URL : 'https://v8.9rnri.com/2048/',
-    ACCOUNT : '',
-    PASSWORD : '',
-    QUESTION : '',
-    ANSWER : '',
+    ACCOUNT : 'bobonwyb',
+    PASSWORD : '010902',
+    QUESTION : '4',
+    ANSWER : '深圳科学高中',
 });
